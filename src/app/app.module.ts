245b73cecba1ac30275cd85dbb3fdb2ea1dd64b3
gerson.carrasco <gerson.carrasco@gestionysistemas.com>
2022-06-25 18:13:42 -0500

    install angular material, theming project, include shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/share.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { MainComponent } from './components/main/main.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
